861cf50acfa938980e9287271a5ac96e2015cc65
hcarneiro <hcarneiro@weboo.biz>
2019-07-05 21:04:37 +0100

    Trying to create stubs automatically

--- test/Card.spec.js
+++ test/Card.spec.js
@@ -1,6 +1,8 @@
 import { shallow, createLocalVue, RouterLinkStub } from '@vue/test-utils'
 import NuxtBuefy from 'nuxt-buefy'
 import Card from '@/components/Card.vue'
+const path = require('path')
+const generateStubs = require(path.resolve(__dirname, './fixture/stubs'))
 
 describe('Card', () => {
   let wrapper
@@ -15,12 +17,22 @@ describe('Card', () => {
         rating: '9.0',
         thumb: 'https://via.placeholder.com/150'
       },
-      stubs: {
-        'nuxt-link': RouterLinkStub,
-        'no-ssr': true,
-        'vue-circle': true
-      }
+      stubs: generateStubs(Card)
     })
+
+    // wrapper = shallow(Card, {
+    //   propsData: {
+    //     title: 'foo',
+    //     tags: ['bar', 'foobar'],
+    //     rating: '9.0',
+    //     thumb: 'https://via.placeholder.com/150'
+    //   },
+    //   stubs: {
+    //     'nuxt-link': RouterLinkStub,
+    //     'no-ssr': true,
+    //     'vue-circle': true
+    //   }
+    // })
   })
 
   afterEach(() => {
