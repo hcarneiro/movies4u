2924d0a9ef8d555e7d203551756aaa894145c7d5
hcarneiro <hcarneiro@weboo.biz>
2019-07-05 19:27:10 +0100

    Home page prototype

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,73 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card
-        title="Free"
-        icon="github-circle"
-      >
-        Open source on <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
+  <div>
+    <section class="hero is-light is-large ss-top-hero" style="background-image: url(/the-martian-wallpaper.jpg)">
+      <div class="ss-hero-screen" />
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            The Martian
+          </h1>
+          <h2 class="subtitle">
+            <nuxt-link to="/">
+              <b-tag rounded>
+                Adventure
+              </b-tag>
+            </nuxt-link>
+            <nuxt-link to="/">
+              <b-tag rounded>
+                Science Fiction
+              </b-tag>
+            </nuxt-link>
+            <nuxt-link to="/">
+              <b-tag rounded>
+                Thriller
+              </b-tag>
+            </nuxt-link>
+          </h2>
 
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">
-          Every
-        </b> component is responsive
-      </card>
-
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">
-          Vue.js
-        </a> and <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
-        No other internal dependency
-      </card>
+          <div class="hero-tools">
+            <b-button rounded>
+              <span>
+                Watch trailer
+              </span>
+              <b-icon icon="play-circle-outline" size="is-small" />
+            </b-button>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="container ss-container">
+      <h2 class="title">
+        In Theaters
+      </h2>
+      <div class="columns">
+        <div class="column">
+          <card
+            title="Avengers: Endgame"
+            :tags="['Action', 'Adventure', 'Science Fiction']"
+            rating="8.4"
+            thumb="https://image.tmdb.org/t/p/w370_and_h556_bestv2/or06FN3Dka5tukK1e9sl16pB3iy.jpg"
+          />
+        </div>
+        <div class="column">
+          <card
+            title="Spider-Man: Far from Home"
+            :tags="['Action', 'Adventure', 'Science Fiction']"
+            rating="7.8"
+            thumb="https://image.tmdb.org/t/p/w370_and_h556_bestv2/2cAc4qH9Uh2NtSujJ90fIAMrw7T.jpg"
+          />
+        </div>
+        <div class="column">
+          <card
+            title="Captain Marvel"
+            :tags="['Action', 'Adventure', 'Science Fiction']"
+            rating="7.0"
+            thumb="https://image.tmdb.org/t/p/w370_and_h556_bestv2/AtsgWhDnHTq68L0lLsUrCnM7TjG.jpg"
+          />
+        </div>
+      </div>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
@@ -45,7 +75,6 @@ import Card from '~/components/Card'
 
 export default {
   name: 'HomePage',
-
   components: {
     Card
   }
