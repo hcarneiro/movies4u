d80a4bf86b4be8fbbac732610fbdd7179ccd6ffd
hcarneiro <hcarneiro@weboo.biz>
2019-07-05 19:26:56 +0100

    Default layout for pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,53 +1,90 @@
 <template>
   <div>
     <nav
-      class="navbar header has-shadow is-primary"
+      class="navbar header ss-navbar"
+      :class="{ 'is-scrolled': isScrolled }"
       role="navigation"
       aria-label="main navigation"
     >
       <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="/"
-        >
+        <nuxt-link to="/" class="navbar-item">
           <img
-            src="~assets/buefy.png"
-            alt="Buefy"
-            height="28"
+            src="~assets/logo.png"
+            alt="Logo"
           >
-        </a>
+        </nuxt-link>
 
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="ss-main-menu">
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+        </a>
+      </div>
+
+      <div id="ss-main-menu" class="navbar-menu">
+        <div class="navbar-start">
+          <nuxt-link to="/movies" class="navbar-item">
+            Movies
+          </nuxt-link>
+          <nuxt-link to="/tv-shows" class="navbar-item">
+            TV Shows
+          </nuxt-link>
+          <nuxt-link to="/news" class="navbar-item">
+            News
+          </nuxt-link>
+        </div>
+        <div class="navbar-end">
+          <b-dropdown position="is-bottom-left" aria-role="menu">
+            <a
+              slot="trigger"
+              class="navbar-item"
+              role="button"
+            >
+              <span>Login</span>
+              <b-icon icon="menu-down" />
+            </a>
+
+            <b-dropdown-item aria-role="menu-item" custom paddingless>
+              <form action="">
+                <div class="modal-card" style="width:300px;">
+                  <section class="modal-card-body">
+                    <b-field label="Email">
+                      <b-input
+                        type="email"
+                        placeholder="Your email"
+                        required
+                      />
+                    </b-field>
+
+                    <b-field label="Password">
+                      <b-input
+                        type="password"
+                        password-reveal
+                        placeholder="Your password"
+                        required
+                      />
+                    </b-field>
+
+                    <b-checkbox>Remember me</b-checkbox>
+                  </section>
+                  <footer class="modal-card-foot">
+                    <button class="button is-primary">
+                      Login
+                    </button>
+                  </footer>
+                </div>
+              </form>
+            </b-dropdown-item>
+          </b-dropdown>
+          <nuxt-link to="/signup" class="navbar-item">
+            Signup
+          </nuxt-link>
         </div>
       </div>
     </nav>
 
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key"
-          >
-            <nuxt-link
-              :to="item.to"
-              exact-active-class="is-active"
-            >
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
-        <nuxt />
-      </div>
+    <section class="main-content ss-content">
+      <nuxt />
     </section>
   </div>
 </template>
@@ -56,18 +93,45 @@
 export default {
   data() {
     return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' }
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' }
-        }
-      ]
+      isScrolled: false
+    }
+  },
+  mounted() {
+    this.attachHandlers()
+  },
+  methods: {
+    burgerMenuHandler() {
+      // Get all "navbar-burger" elements
+      const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
+
+      // Check if there are any navbar burgers
+      if ($navbarBurgers.length > 0) {
+        // Add a click event on each of them
+        $navbarBurgers.forEach((el) => {
+          el.addEventListener('click', () => {
+            // Get the target from the "data-target" attribute
+            const target = el.dataset.target
+            const $target = document.getElementById(target)
+
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            el.classList.toggle('is-active')
+            $target.classList.toggle('is-active')
+          })
+        })
+      }
+    },
+    onScroll() {
+      const top = window.pageYOffset
+
+      if (top >= 20) {
+        this.isScrolled = true
+      } else {
+        this.isScrolled = false
+      }
+    },
+    attachHandlers() {
+      this.burgerMenuHandler()
+      window.addEventListener('scroll', this.onScroll)
     }
   }
 }
