5649e1c2949a183630ff51c0b603c52f5e05aa33
hcarneiro <hcarneiro@weboo.biz>
2019-07-05 20:55:46 +0100

    Changes

--- test/Card.spec.js
+++ test/Card.spec.js
@@ -1,23 +1,11 @@
 import { shallow, createLocalVue, RouterLinkStub } from '@vue/test-utils'
-import { Nuxt, Builder } from 'nuxt'
 import NuxtBuefy from 'nuxt-buefy'
 import Card from '@/components/Card.vue'
 
-const config = require('./fixture/nuxt.config')
-
 describe('Card', () => {
   let wrapper
-  let nuxt
-
-  beforeAll(async () => {
-    nuxt = new Nuxt(config)
-    await new Builder(nuxt).build()
-    await nuxt.listen(3000)
-  }, 60000)
-
-  afterAll(async () => {
-    await nuxt.close()
-  })
+  const localVue = createLocalVue()
+  localVue.use(NuxtBuefy)
 
   beforeEach(() => {
     wrapper = shallow(Card, {
@@ -40,8 +28,6 @@ describe('Card', () => {
   })
 
   test('is a Vue instance', () => {
-    const localVue = createLocalVue()
-    localVue.use(NuxtBuefy)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
