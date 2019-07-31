19bb91c3730a8d4e07c353c0c6d65d29d5fc7aaf
Hugo Carneiro <hello@thegeekdeveloper.com>
2019-07-31 17:07:55 +0100

    Updated validator schema

--- api/v1/validators/userSignup.js
+++ api/v1/validators/userSignup.js
@@ -1,26 +1,33 @@
 module.exports = {
   firstName: {
-    notEmpty: {
+    isEmpty: {
+      negated: true,
       errorMessage: 'First name is required'
     }
   },
   lastName: {
-    notEmpty: {
+    isEmpty: {
+      negated: true,
       errorMessage: 'Last name is required'
     }
   },
   email: {
+    isEmpty: {
+      negated: true,
+      errorMessage: 'Email is required'
+    },
     isEmail: {
       errorMessage: 'Email address is invalid'
     }
   },
   password: {
-    notEmpty: {
+    isEmpty: {
+      negated: true,
       errorMessage: 'Password is required'
     },
     isLength: {
-      options: [{ min: 8, max: 64 }],
-      errorMessage: 'Password must be between 8 and 64 characters long'
+      errorMessage: 'Password must be between 8 and 64 characters long',
+      options: { min: 8, max: 64 }
     }
   }
 }
