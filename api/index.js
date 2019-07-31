a3bc8bceb8c0f1168c2bb36b8211d58ec3b714fd
Hugo Carneiro <hello@thegeekdeveloper.com>
2019-07-31 19:52:36 +0100

    Filter fields to show

--- api/index.js
+++ api/index.js
@@ -1,12 +1,15 @@
+const _ = require('lodash')
 const express = require('express')
 const router = express.Router()
 const appPackage = require('../package')
+
+const userAttributes = ['id', 'email', 'auth_token', 'firstName', 'lastName', 'preferences', 'userBio', 'userCountry', 'userCity', 'profilePicture', 'createdAt', 'facebookId', 'googleId']
 
 router.get('/', (req, res) => {
   let user
 
   if (req.user) {
-    user = req.user.get({ plain: true })
+    user = _.pick(req.user, userAttributes)
     user.auth_token = req.auth_token
   }
 
