e5699b860a79ff1a66f089daea6c40ab6ad0683c
Hugo Carneiro <hello@thegeekdeveloper.com>
2019-08-27 17:49:03 +0100

    Site tile updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - Movie lists to watch',
+    titleTemplate: '%s - That Movie List',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
