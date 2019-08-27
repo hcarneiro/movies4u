ae4b5d409318918b55aa84846bee34dd032552ec
Hugo Carneiro <hello@thegeekdeveloper.com>
2019-08-27 17:48:25 +0100

    Handle redirect when logged in or out

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,25 @@
-const routesToCheckAuth = [
+const routesToCheckIfVerified = [
   'signup',
   'verify-token',
   'reset-password-token',
   'forgot-password'
 ]
 
+const routesToCheckIfNotVerified = [
+  'account'
+]
+
 export default function ({ store, route, redirect }) {
-  if (routesToCheckAuth.includes(route.name) && store.state.auth.verified) {
+  const check = routesToCheckIfNotVerified.some((path) => {
+    const test = (new RegExp(path, 'gi')).test(route.path)
+    return !!test
+  })
+
+  if (check && !store.state.auth.verified) {
+    return redirect('301', '/')
+  }
+
+  if (routesToCheckIfVerified.includes(route.name) && store.state.auth.verified) {
     return redirect('301', '/')
   }
 }
