e8d445e58c7ad3637356b926ce6aef1adff5c32d
Hugo Carneiro <hello@thegeekdeveloper.com>
2019-08-28 21:04:57 +0100

    Update media.js

--- store/media.js
+++ store/media.js
@@ -4,14 +4,9 @@ export const mutations = {}
 
 export const actions = {
   uploadThumb({ commit }, file) {
-    const projectData = {
-      thumb: ''
-    }
-
     return this.$axios.post(`/api/v1/upload/thumb`, file)
       .then((res) => {
         if (res.status === 200) {
-          projectData.thumb = res.data.url
           return res.data
         }
       })
