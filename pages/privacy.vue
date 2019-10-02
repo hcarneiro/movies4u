06fc185efadd340c333608245c70b392aca3f0d2
Hugo Carneiro <hello@thegeekdeveloper.com>
2019-10-02 19:08:57 +0100

    Privacy and Terms page

---
+++ pages/privacy.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <section class="hero">
+      <div class="hero-body ss-account-hero">
+        <div class="container">
+          <article>
+            <div class="content">
+              <h1 class="title">
+                Privacy Policy
+              </h1>
+            </div>
+          </article>
+        </div>
+      </div>
+    </section>
+    <div class="container ss-detail-container">
+      Policy here soon
+    </div>
+  </div>
+</template>
